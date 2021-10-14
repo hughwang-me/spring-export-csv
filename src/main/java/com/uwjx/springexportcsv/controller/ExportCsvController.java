e44a3eef5ec65de001f5ea99fa64df9995dcdfca
王欢 <18501667737@163.com>
2021-10-14 14:28:04 +0800

    by bash

--- src/main/java/com/uwjx/springexportcsv/controller/ExportCsvController.java
+++ src/main/java/com/uwjx/springexportcsv/controller/ExportCsvController.java
@@ -3,6 +3,8 @@ package com.uwjx.springexportcsv.controller;
 import com.alibaba.fastjson.JSON;
 import com.uwjx.springexportcsv.bean.ReqBean;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,10 +15,7 @@ import org.springframework.web.servlet.function.EntityResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URLEncoder;
 
 /**
@@ -80,6 +79,78 @@ public class ExportCsvController {
             log.warn("JSOS:{}"  , JSON.toJSONString(reqBean));
             String fileName = "导出的文件.csv";
             File file = new File("/Users/wanghuan/Documents/tanli_workspace/tanli-levo-uniapp/push.sh");
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", URLEncoder.encode(fileName,"utf-8"));
+            return new ResponseEntity<>
+                    (FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @PostMapping(value = "csv4")
+    public ResponseEntity<byte[]> exportCsv4(@RequestBody ReqBean reqBean){
+        try {
+            log.warn("JSOS:{}"  , JSON.toJSONString(reqBean));
+
+            String filePath = "/Users/wanghuan/Documents/rx_workspace/spring-export-csv/a.csv";
+
+            FileOutputStream fos = new FileOutputStream(filePath);
+            OutputStreamWriter osw = new OutputStreamWriter(fos, "GBK");
+
+            CSVFormat csvFormat = CSVFormat.Builder.create()
+                    .setHeader("head1" , "head2")
+                    .build();
+            CSVPrinter csvPrinter = new CSVPrinter(osw, csvFormat);
+
+            for (int i = 0; i < 10; i++) {
+                csvPrinter.printRecord("张三"+i, 20+i);
+            }
+
+            csvPrinter.flush();
+            csvPrinter.close();
+            fos.close();
+            osw.close();
+
+            String fileName = "导出的文件.csv";
+            File file = new File(filePath);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", URLEncoder.encode(fileName,"utf-8"));
+            return new ResponseEntity<>
+                    (FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @GetMapping(value = "csv5")
+    public ResponseEntity<byte[]> exportCsv5(){
+        try {
+            String filePath = "/Users/wanghuan/Documents/rx_workspace/spring-export-csv/a.csv";
+
+            FileOutputStream fos = new FileOutputStream(filePath);
+            OutputStreamWriter osw = new OutputStreamWriter(fos, "GBK");
+
+            CSVFormat csvFormat = CSVFormat.Builder.create()
+                    .setHeader("head1" , "head2")
+                    .build();
+            CSVPrinter csvPrinter = new CSVPrinter(osw, csvFormat);
+
+            for (int i = 0; i < 10; i++) {
+                csvPrinter.printRecord("张三"+i, 20+i);
+            }
+
+            csvPrinter.flush();
+            csvPrinter.close();
+            fos.close();
+            osw.close();
+
+            String fileName = "导出的文件.csv";
+            File file = new File(filePath);
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
             headers.setContentDispositionFormData("attachment", URLEncoder.encode(fileName,"utf-8"));
